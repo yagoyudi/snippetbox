f2ea1019179ff98ba4ed4ead326025f86451812e
Yago Furuta <yagofuruta@protonmail.com>
2024-09-21 09:33:30 -0300

    feat: stateful http

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -55,9 +55,10 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 }
 
 // newTemplateData returns a new template data with the current year.
-func (app *application) newTemplateData(_ *http.Request) *templateData {
+func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
 		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
