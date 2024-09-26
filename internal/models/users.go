98b91fb9bc781af1e3ccffcf909d182c8f0bd760
Yago Furuta <yagofuruta@protonmail.com>
2024-09-26 16:22:02 -0300

    fix: syntax error

--- internal/models/users.go
+++ internal/models/users.go
@@ -76,7 +76,7 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 func (m *UserModel) Exists(id int) (bool, error) {
 	var exists bool
 
-	stmt := "SELECT EXISTS(SELEC true FROM users WHERE id = ?)"
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
 	err := m.DB.QueryRow(stmt, id).Scan(&exists)
 	return exists, err
 }
