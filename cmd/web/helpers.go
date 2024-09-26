c76564f32be6af357649f975df7f55358e872343
Yago Furuta <yagofuruta@protonmail.com>
2024-09-26 08:04:33 -0300

    feat: using context pkg

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -85,5 +85,9 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 
 // isAuthenticated returns true if the request r is from an authenticated user.
 func (app *application) isAuthenticated(r *http.Request) bool {
-	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+	isAuthenticated, ok := r.Context().Value(isAuthenticatedContextKey).(bool)
+	if !ok {
+		return false
+	}
+	return isAuthenticated
 }
