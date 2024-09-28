7c4f4d21375eb95021839cf29ef1c4055f182fde
Yago Furuta <yagofuruta@protonmail.com>
2024-09-28 10:52:21 -0300

    feat: change password

--- internal/models/users.go
+++ internal/models/users.go
@@ -15,6 +15,7 @@ type UserModelInterface interface {
 	Authenticate(email, password string) (id int, err error)
 	Exists(id int) (bool, error)
 	Get(id int) (*User, error)
+	PasswordUpdate(id int, currentPassword, newPassword string) error
 }
 
 // User type matches the "users" table.
@@ -100,4 +101,32 @@ func (m *UserModel) Get(id int) (*User, error) {
 		}
 	}
 	return &user, nil
+}
+
+func (m *UserModel) PasswordUpdate(id int, currentPassword, newPassword string) error {
+	currentHashedPassword := []byte{}
+	stmt := "SELECT hashed_password FROM users WHERE id = ?"
+	err := m.DB.QueryRow(stmt, id).Scan(&currentHashedPassword)
+	if err != nil {
+		return err
+	}
+
+	err = bcrypt.CompareHashAndPassword(currentHashedPassword, []byte(currentPassword))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return ErrInvalidCredentials
+		} else {
+			return err
+		}
+	}
+
+	newHashedPassword, err := bcrypt.GenerateFromPassword([]byte(newPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt = "UPDATE users SET hashed_password = ? WHERE id = ?"
+
+	_, err = m.DB.Exec(stmt, string(newHashedPassword), id)
+	return err
 }
